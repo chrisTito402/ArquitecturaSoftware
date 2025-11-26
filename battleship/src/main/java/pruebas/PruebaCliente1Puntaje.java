695c21b7ccbf521df03fa72ff908ed4be302d42b
FreddyACastroR <freddy.castro252191@potros.itson.edu.mx>
2025-11-26 03:31:03 -0700

    Pruebas (ignorar)

---
+++ battleship/src/main/java/pruebas/PruebaCliente1Puntaje.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebas;
+
+import buseventos.Mensaje;
+import buseventos.TipoAccion;
+import clientesocket.ClienteSocket;
+import com.google.gson.Gson;
+import controllers.controller.ControlVista;
+import controllers.controller.Controlador;
+import java.util.ArrayList;
+import java.util.HashMap;
+import models.control.ControlModelo;
+import models.enums.ColorJugador;
+import models.enums.EstadoJugador;
+import views.DTOs.JugadorDTO;
+import views.DTOs.TableroDTO;
+import views.frames.TimerPanel;
+
+public class PruebaCliente1Puntaje {
+
+    public static void main(String[] args) {
+        ControlVista cV = ControlVista.getInstancia();
+        
+        JugadorDTO jugador = new JugadorDTO("Jugador1", ColorJugador.AZUL, EstadoJugador.JUGANDO);
+        TableroDTO tablero = new TableroDTO(10, 10);
+        
+        ControlModelo cM = new ControlModelo(jugador, tablero, new ArrayList<>(), true, new ArrayList<>());
+        cM.suscribirAPartida(cV);
+        
+        ClienteSocket cS = new ClienteSocket("localhost", 5000, null);
+        Controlador c = new Controlador(cM, cS, new HashMap<>());
+        cS.setControl(c);
+        cV.setControl(c);
+        
+        cV.setTimer(new TimerPanel(1000, 30));
+        cV.initTableroPropio();
+        cV.initTableroEnemigo();
+        cV.mostrarFrmPartidaEnCurso();
+        
+        cS.execute();
+        
+        Mensaje m = new Mensaje(TipoAccion.SUSCRIBIR, "RESULTADO_DISPARO", null, "1");
+        Gson gson = new Gson();
+        String json = gson.toJson(m);
+        cS.enviarMensaje(json);
+        
+        System.out.println("========================================");
+        System.out.println("Cliente 1 iniciado: " + jugador.getNombre());
+        System.out.println("Panel de puntaje activo");
+        System.out.println("========================================");
+    }
+}
