f48e3f5098022d9e69b3a8734fc94dce75eaabb6
DanielBuelnaAndujo <daniel.buelna260378@potros.itson.edu.mx>
2025-11-30 14:38:56 -0700

    Validaciones de AddNave en ControlModelo

--- battleship/src/main/java/models/control/ControlModelo.java
+++ battleship/src/main/java/models/control/ControlModelo.java
@@ -1,9 +1,11 @@
 package models.control;
 
 import java.time.Instant;
+import java.util.Comparator;
 import java.util.List;
 import models.entidades.Coordenadas;
 import models.entidades.Jugador;
+import models.enums.OrientacionNave;
 import models.enums.ResultadoAddNave;
 import models.enums.ResultadoDisparo;
 import models.observador.ISuscriptor;
@@ -71,6 +73,78 @@ public class ControlModelo implements IModeloCliente {
 
     @Override
     public AddNaveDTO addNave(NaveDTO nave, List<Coordenadas> coordenadas) {
+        // Verificar que la nave no sea nula.
+        if (nave == null) {
+            System.out.println("Error, nave vacia.");
+            return null;
+        }
+        // Verificar que las coordenadas no esten vacias.
+        if (coordenadas == null || coordenadas.isEmpty()) {
+            System.out.println("Error, coordenadas vascias.");
+            return null;
+        }
+        
+        // Verificar que el numero de coordenadas sea el mismo que el tamaño de la Nave.
+        if (coordenadas.size() != nave.getTamanio()) {
+            System.out.println("Error, coordenadas extra o insuficientes para la Nave.");
+            return null;
+        }
+        
+        // Verificar que las coordenadas no se salen del limite del tablero.
+        for (Coordenadas coordenada : coordenadas) {
+            if (coordenada.getY() < 0 || coordenada.getY() > tablero.getLimiteY()
+                    || coordenada.getX() < 0 || coordenada.getX() > tablero.getLimiteX()) {
+                System.out.println("Error: La nave se sale de los limites del tablero.");
+                return null;
+            }
+        }
+        
+        // Verificar que todas las coordenas esten con la misma orientacion.
+        if (nave.getOrientacion() == OrientacionNave.VERTICAL) {
+            int y = coordenadas.getFirst().getY();
+            for (Coordenadas coordenada : coordenadas) {
+                if (coordenada.getY() != y) {
+                    System.out.println("Error: La nave no esta ordenada Verticalmente");
+                    return null;
+                }
+            }
+        } else if (nave.getOrientacion() == OrientacionNave.HORIZONTAL) {
+            int x = coordenadas.getFirst().getX();
+            for (Coordenadas coordenada : coordenadas) {
+                if (coordenada.getX() != x) {
+                    System.out.println("Error: La nave no esta ordenada Horizontalmente.");
+                    return null;
+                }
+            }
+        }
+        
+        // Ordenar lista por "X" y "Y".
+        coordenadas.sort(Comparator.comparingInt(Coordenadas::getX)
+                .thenComparingInt(Coordenadas::getY));
+
+        // Verificar que cada Coordenada este consecutiva de la otra.
+        if (nave.getOrientacion() == OrientacionNave.VERTICAL) {
+            for (int i = coordenadas.size() - 1; i >= 0; i--) {
+                if (i == 0) {
+                    break;
+                }
+                if (coordenadas.get(i - 1).getX() != coordenadas.get(i).getX() - 1) {
+                    System.out.println("Error: Coordenas no consecutivas en 'X'");
+                    return null;
+                }
+            }
+        } else if (nave.getOrientacion() == OrientacionNave.HORIZONTAL) {
+            for (int i = coordenadas.size() - 1; i >= 0; i--) {
+                if (i == 0) {
+                    break;
+                }
+                if (coordenadas.get(i - 1).getY() != coordenadas.get(i).getY() - 1) {
+                    System.out.println("Error: Coordenas no consecutivas en 'Y'");
+                    return null;
+                }
+            }
+        }
+        
         AddNaveDTO naveDTO = new AddNaveDTO(
                 jugador, 
                 nave, 
